0701aed96d68ac0b9b4c9e412fc8f3e885e7e505
harisince1996 <harisince1996@gmail.com>
2023-08-15 11:48:55 +0530

    finial down by ram

--- src/org/git/CopyGit.java
+++ src/org/git/CopyGit.java
@@ -10,10 +10,11 @@ public static void main(String[] args) {
 	System.out.println("copy git");	
 	String s= "phython";
 	String r="";
-for (int i = 0; i < args.length; i++) {
-	System.out.println("final step");
-	
+for (int i = s.length()-1; i >=0 ; i--) {
+	char ch = s.charAt(i);
+	r=r+ch;
 	}
+System.out.println("after rev:"+r);
 
 }
 }
